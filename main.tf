d9d08dc0abdaea0ed8945d512b1bb196bbcdf4b4
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-03-01 19:10:21 -0300

    private endpoints

--- main.tf
+++ main.tf
@@ -40,15 +40,15 @@ resource "azurerm_subnet" "main" {
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.1.0/24"]
 
-  delegation {
-    name = "delegation"
-    service_delegation {
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/action",
-      ]
-      name = "Microsoft.Web/serverFarms"
-    }
-  }
+  # delegation {
+  #   name = "delegation"
+  #   service_delegation {
+  #     actions = [
+  #       "Microsoft.Network/virtualNetworks/subnets/action",
+  #     ]
+  #     name = "Microsoft.Web/serverFarms"
+  #   }
+  # }
 }
 
 resource "azurerm_log_analytics_workspace" "main" {
