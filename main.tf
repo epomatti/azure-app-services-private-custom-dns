d9bd1c574faa75d7ba735fd367c88dfc84a6d211
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-03-04 10:04:24 -0300

    nit

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     azurerm = {
@@ -50,7 +49,6 @@ resource "azurerm_virtual_network" "main" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-
 }
 
 resource "azurerm_subnet" "main" {
